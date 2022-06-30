2db04107ca2568a3bb72d70d7bc22c8600259aff
Raphael Ezeigwe <iraphael1308@gmail.com>
2022-06-30 21:57:57 +0100

    style changes

--- routes/apple_pay.py
+++ routes/apple_pay.py
@@ -11,14 +11,6 @@ class ApplePay(Request):
     path = '/apple-pay/domain'
 
     def register(self, domain: str):
-        """Register a top-level domain or subdomain for your Apple Pay integration.
-
-        Args:
-            domain (str): Domain name to be registered
-
-        Returns:
-            json
-        """
 
         payload = {
             'domainName': util.check_domain(domain)
@@ -26,22 +18,9 @@ class ApplePay(Request):
         return self.post(self.path, payload=payload)
 
     def list_domains(self):
-        """Lists all registered domains on your integration. Returns an empty array if no domains have been added.
-
-        Returns:
-            json
-        """
         return self.get(self.path)
 
     def unregister(self, domain: str):
-        """Unregister a top-level domain or subdomain previously used for your Apple Pay integration.
-
-        Args:
-            domain (str): Domain name to be unregistered
-
-        Returns:
-            json
-        """
         payload = {
             'domainName': util.check_domain(domain)
         }
